73ff68b6db80afc29cae3a7c799f593fec92a92b
Thakuriprince <princethakuri11111@gmail.com>
2021-06-06 09:45:18 +0545

    'secondupdate'

---
+++ quiz-app/main.js
@@ -0,0 +1,43 @@
+const quizData = [
+  {
+    question: "Grand Central Terminal, Park Avenue, New York is the worlds",
+    a: "largest railway station",
+    b: "highest railway station",
+    c: "longest railway station",
+    d: "None of the above",
+    correct: "a",
+  },
+  {
+    question: "Entomology is the science that studies",
+    a: "Behavior of human beings",
+    b: "Insects",
+    c: "The origin and history of technical and scientific terms",
+    d: "The formation of rocks",
+    correct: "b",
+  },
+  {
+    question:
+      "Eritrea, which became the 182nd member of the UN in 1993, is in the continent of",
+    a: "Asia",
+    b: "Africa",
+    c: "Europe",
+    d: "Australia",
+    correct: "b",
+  },
+  {
+    question: "For which of the following disciplines is Nobel Prize awarded?",
+    a: "Physics and Chemistry",
+    b: "Physiology or Medicine",
+    c: "Literature, Peace and Economics",
+    d: "All of the above",
+    correct: "d",
+  },
+  {
+    question: "Hitler party which came into power in 1933 is known as",
+    a: "Labour Party",
+    b: "Nazi Party",
+    c: "Ku-Klux-Klan",
+    d: "	Democratic Party",
+    correct: "b",
+  },
+];
